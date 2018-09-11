371ebd80854b7c01fb4d550c06938522b646f2e0
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-09-11 15:18:03 -0400

    adding a borough field

---
+++ retailfoodstores_build/sql/borough.sql
@@ -0,0 +1,23 @@
+-- adding a borough field with the borough value for each record
+ALTER TABLE dcp_retailfoodstores
+ADD COLUM borough;
+
+UPDATE dcp_retailfoodstores
+SET borough = 'Bronx'
+WHERE county = 'Bronx';
+
+UPDATE dcp_retailfoodstores
+SET borough = 'Bronx'
+WHERE county = 'Bronx';
+
+UPDATE dcp_retailfoodstores
+SET borough = 'Bronx'
+WHERE county = 'Bronx';
+
+UPDATE dcp_retailfoodstores
+SET borough = 'Bronx'
+WHERE county = 'Bronx';
+
+UPDATE dcp_retailfoodstores
+SET borough = 'Bronx'
+WHERE county = 'Bronx';
