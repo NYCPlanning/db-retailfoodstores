091f9ffd0c3b5747601ecf844c7d4452f7581660
tommyswang <tommyswang@gmail.com>
2018-09-14 17:38:16 -0400

    Added parse lib and parse function

--- retailfoodstores_build/python/geocode_address.py
+++ retailfoodstores_build/python/geocode_address.py
@@ -3,7 +3,11 @@ import subprocess
 import os
 import sqlalchemy as sql
 import json
+import usaddress
 from nyc_geoclient import Geoclient
+
+# If you get this error: requests.exceptions.ConnectionError: ('Connection aborted.', error(54, 'Connection reset by peer')) install below or try the other solutions in the link
+# pip install pyopenssl idna (https://github.com/jakubroztocil/httpie/issues/351)
 
 # make sure we are at the top of the repo
 wd = subprocess.check_output('git rev-parse --show-toplevel', shell = True)
@@ -65,6 +69,30 @@ for i in range(len(retail)):
     elif locs['lat'][i] == 'none':
         upd = "UPDATE dcp_retailfoodstores a SET geom = NULL WHERE a.license_number = '" + retail['license_number'][i] + "';"
     engine.execute(upd)
+
+
+# Read in retail stores table where values of geom is null and street_number is not null
+retail_2 = pd.read_sql_query('SELECT street_number, location, borough FROM dcp_retailfoodstores WHERE street_number IS NOT NULL AND street_name IS NOT NULL AND geom IS NULL;', engine)
+
+# https://github.com/datamade/usaddress (repo for python usaddress lib)
+# Parse the location value to get a more accurate street name
+def parse_name(location):
+    parsed = usaddress.parse(location)
+    parsed_concat = ''
+    if (parsed[0][1] == 'StreetNamePreDirectional'):
+        parsed_concat += parsed[0][0]
+        if (parsed[1][1] == 'StreetName'):
+            parsed_concat += parsed[1][0]
+            if (parsed[2][1] == 'StreetNamePostType'):
+                parsed_concat += parsed[2][0]
+
+    if(parsed[0][1] == 'StreetName'):
+        parsed_concat += parsed[0][0]
+        if (parsed[1][1] == 'StreetNamePostType'):
+            parsed_concat += parsed[1][0]
+            
+    # returns the street name as a string
+    return(parsed_concat)
 
 # not deleting because if I ever figure it out this is probably a better way of doing this...
 #md = sql.MetaData(engine)
