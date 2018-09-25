46bc1c5b3ccaef099c9278c21fbc5055c6af133d
tommyswang <tommyswang@gmail.com>
2018-09-25 11:11:07 -0400

    Added more outliers for the square_footage column

--- retailfoodstores_build/sql/qc_squarefootage.sql
+++ retailfoodstores_build/sql/qc_squarefootage.sql
@@ -1,3 +1,3 @@
 -- Exports all the records with a 0 square footage to a new file
-COPY (SELECT * FROM dcp_retailfoodstores WHERE square_footage = 0)
-TO '/Users/tommywang/git/db-retailfoodstores/retailfoodstores_build/output/retailfoodstores_0_square_footage.csv' DELIMITER ',' CSV HEADER;
+COPY (SELECT * FROM dcp_retailfoodstores WHERE square_footage <= 50)
+TO '/Users/tommywang/git/db-retailfoodstores/retailfoodstores_build/output/retailfoodstores_square_footage.csv' DELIMITER ',' CSV HEADER;
