8ca6a558721e82848afeb784e548856c1f764164
tommyswang <tommyswang@gmail.com>
2018-09-28 11:42:59 -0400

    Joined new data with existing table

---
+++ retailfoodstores_build/sql/stores_join.sql
@@ -0,0 +1,27 @@
+-- Join the table from sam to the geocoded table
+ALTER TABLE dcp_retailfoodstores
+ADD COLUMN dcp_type text,
+ADD COLUMN dcp_square_footage int;
+
+-- Update the new square footage from sam's table into the geocoded table that has changed
+UPDATE dcp_retailfoodstores r
+SET dcp_square_footage = g.sf
+FROM dcp_grocerystores g
+WHERE g.license = r.license_number
+AND g.sf != r.square_footage;
+
+-- Update the grocery category type of the stores into the geocoded table
+UPDATE dcp_retailfoodstores r
+SET dcp_type = 'Grocery'
+FROM  dcp_grocerystores g
+WHERE g.grocery = 'Yes'
+AND g.sf <= 2000
+AND g.license = r.license_number;
+
+-- Update the supermarket category type of the stores into the geocoded table
+UPDATE dcp_retailfoodstores r
+SET dcp_type = 'Supermarket'
+FROM  dcp_grocerystores g
+WHERE g.grocery = 'Yes'
+AND 2000 < g.sf
+AND g.license = r.license_number;
